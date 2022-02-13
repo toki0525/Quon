31dcb2d1b21abd09223264dabfbb10a9a54b077a
NEW[ねう] <56794853+toki0525@users.noreply.github.com>
2022-02-13 13:08:06 +0900

    Add files via upload

---
+++ home/common.js
@@ -0,0 +1,9 @@
+/* global $ */
+
+$(function(){
+  $("#slider").slick({
+        dots: true,
+        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"></button>'
+  });
+});
